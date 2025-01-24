7cbc8e2ccfaef79b282b9fef979789f4fc16724f
kghamilton89 <29099829+kghamilton89@users.noreply.github.com>
2025-01-24 19:00:46 +0300

    reduce output verbosity

--- scrape.py
+++ scrape.py
@@ -54,54 +54,29 @@ def parse_listing(keyword, place, page):
 
                 for results in listings:
                     XPATH_BUSINESS_NAME = ".//a[@class='business-name']//text()"
-                    XPATH_BUSSINESS_PAGE = ".//a[@class='business-name']//@href"
                     XPATH_TELEPHONE = ".//div[@class='phones phone primary']//text()"
-                    XPATH_STREET = ".//div[@class='street-address']//text()"
-                    XPATH_LOCALITY = ".//div[@class='locality']//text()"
-                    XPATH_REGION = ".//div[@class='info']//div//p[@itemprop='address']//span[@itemprop='addressRegion']//text()"
                     XPATH_ZIP_CODE = ".//div[@class='info']//div//p[@itemprop='address']//span[@itemprop='postalCode']//text()"
                     XPATH_RANK = ".//div[@class='info']//h2[@class='n']/text()"
-                    XPATH_CATEGORIES = ".//div[@class='info']//div[contains(@class,'info-section')]//div[@class='categories']//text()"
                     XPATH_WEBSITE = ".//div[@class='info']//div[contains(@class,'info-section')]//div[@class='links']//a[contains(@class,'website')]/@href"
-                    XPATH_RATING = ".//div[@class='info']//div[contains(@class,'info-section')]//div[contains(@class,'result-rating')]//span//text()"
 
                     raw_business_name = results.xpath(XPATH_BUSINESS_NAME)
                     raw_business_telephone = results.xpath(XPATH_TELEPHONE)
-                    raw_business_page = results.xpath(XPATH_BUSSINESS_PAGE)
-                    raw_categories = results.xpath(XPATH_CATEGORIES)
                     raw_website = results.xpath(XPATH_WEBSITE)
-                    raw_rating = results.xpath(XPATH_RATING)
-                    raw_street = results.xpath(XPATH_STREET)
-                    raw_locality = results.xpath(XPATH_LOCALITY)
-                    raw_region = results.xpath(XPATH_REGION)
                     raw_zip_code = results.xpath(XPATH_ZIP_CODE)
                     raw_rank = results.xpath(XPATH_RANK)
 
                     business_name = ''.join(raw_business_name).strip() if raw_business_name else None
                     telephone = ''.join(raw_business_telephone).strip() if raw_business_telephone else None
-                    business_page = ''.join(raw_business_page).strip() if raw_business_page else None
                     rank = ''.join(raw_rank).replace('.\xa0', '') if raw_rank else None
-                    category = ','.join(raw_categories).strip() if raw_categories else None
                     website = ''.join(raw_website).strip() if raw_website else None
-                    rating = ''.join(raw_rating).replace("(", "").replace(")", "").strip() if raw_rating else None
-                    street = ''.join(raw_street).strip() if raw_street else None
-                    locality = ''.join(raw_locality).replace(',\xa0', '').strip() if raw_locality else None
-                    region = ''.join(raw_region).strip() if raw_region else None
                     zipcode = ''.join(raw_zip_code).strip() if raw_zip_code else None
 
                     business_details = {
                         'business_name': business_name,
                         'telephone': telephone,
-                        'business_page': business_page,
                         'rank': rank,
-                        'category': category,
                         'website': website,
-                        'rating': rating,
-                        'street': street,
-                        'locality': locality,
-                        'region': region,
                         'zipcode': zipcode,
-                        'listing_url': response.url
                     }
                     scraped_results.append(business_details)
 
@@ -163,16 +138,12 @@ if __name__ == "__main__":
             time.sleep(10)
 
 if all_scraped_data:
-    # Replace ", " with "-" in the place name for the filename
     formatted_place = place.replace(", ", "-")
     file_name = f"{keyword}-{formatted_place}-yellowpages.csv"
     print(f"Writing scraped data to {file_name}")
     with open(file_name, 'wb') as csvfile:
-        fieldnames = ['rank', 'business_name', 'telephone', 'business_page', 'category', 'website', 'rating',
-                      'street', 'locality', 'region', 'zipcode', 'listing_url']
+        fieldnames = ['rank', 'business_name', 'telephone', 'website', 'zipcode']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
         writer.writeheader()
         for data in all_scraped_data:
             writer.writerow(data)
-
-            print(f"Scraping complete. Data has been written to {file_name}")
